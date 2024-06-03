45c9d89454498e29ca21ad0175a80752e22565c5
HariHaaran3 <harihaaran.vijayakumar@gmail.com>
2024-06-03 11:12:02 +0530

    Update UserServiceImpl.java
    Introducing cache

--- src/main/java/com/example/trainbooking/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/example/trainbooking/serviceImpl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.trainbooking.serviceImpl;
 
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,12 +13,22 @@ import com.example.trainbooking.model.UserDetailResponse;
 import com.example.trainbooking.repository.UserDetailRepository;
 import com.example.trainbooking.service.UserService;
 import com.example.trainbooking.util.UserDetailMapper;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserDetailRepository userDetailRepository;
+
+    private Cache<Long, UserDetailResponse> userCache;
+
+    public UserServiceImpl() {
+        userCache = CacheBuilder.newBuilder()
+                                .expireAfterWrite(10, TimeUnit.MINUTES)
+                                .build();
+    }
 
     @Override
     public UserDetailResponse createUser(UserDetailRequest userDetailRequest) {
@@ -32,17 +43,29 @@ public class UserServiceImpl implements UserService {
 
         UserDetail userDetail = UserDetailMapper.toEntity(userDetailRequest);
         UserDetail savedUserDetail = userDetailRepository.save(userDetail);
-        return UserDetailMapper.toResponse(savedUserDetail);
+        UserDetailResponse userDetailResponse = UserDetailMapper.toResponse(savedUserDetail);
+        userCache.put(savedUserDetail.getUserId(), userDetailResponse);
+        return userDetailResponse;
     }
 
     @Override
     public UserDetailResponse getUser(Long userId) {
+        UserDetailResponse cachedUserDetail = userCache.getIfPresent(userId);
+        if (cachedUserDetail != null) {
+            return cachedUserDetail;
+        }
+
         Optional<UserDetail> userDetail = userDetailRepository.findById(userId);
-        return userDetail.map(UserDetailMapper::toResponse).orElse(null);
+        UserDetailResponse userDetailResponse = userDetail.map(UserDetailMapper::toResponse).orElse(null);
+        if (null != userDetailResponse && null != userDetailResponse.getUserId()) {
+            userCache.put(userId, userDetailResponse);
+        }
+        return userDetailResponse;
     }
 
     @Override
     public void deleteUser(Long userId) {
         userDetailRepository.deleteById(userId);
+        userCache.invalidate(userId);
     }
 }
