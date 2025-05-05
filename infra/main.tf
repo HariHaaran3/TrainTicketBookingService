bb3c11efdf962bfb6934e412174e748c0c72e539
MONA\harih <harihaaran.vijayakumar@gmail.com>
2025-05-05 07:06:01 +0530

    target_type  = "ip"

--- infra/main.tf
+++ infra/main.tf
@@ -87,6 +87,7 @@ resource "aws_lb_target_group" "this" {
   port     = 9090
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+  target_type  = "ip"
 
   health_check {
     path                = "/actuator/health"
