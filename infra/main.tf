1eed9e8c1dffb3be94b35d923141454c52558b35
MONA\harih <harihaaran.vijayakumar@gmail.com>
2025-05-05 07:00:53 +0530

    aws_internet_gateway.this,  aws_route_table.public, aws_route_table_association.public

--- infra/main.tf
+++ infra/main.tf
@@ -11,11 +11,30 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "public" {
-  count = 2
+resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.main.id
-  cidr_block = element(["10.0.0.0/24", "10.0.1.0/24"], count.index)
-  availability_zone = element(["ap-south-1a", "ap-south-1b"], count.index)
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(["10.0.0.0/24", "10.0.1.0/24"], count.index)
+  availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
   map_public_ip_on_launch = true
 }
 
